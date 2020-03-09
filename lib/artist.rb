20d4964e2d406d5fba74bcbb64ae8a7a3821a9e9
Terry Threatt <terry.threatt@gmail.com>
2020-03-09 19:39:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,20 +23,7 @@ class Artist
   end 
   
   def self.find_or_create_by_name(name)
-    if @@all.find do |artist|
-      # binding.pry 
-      artist.name == name 
-      artist
-    end 
-    elsif  
-      artist = Artist.new(name)
-      @@all << artist
-      artist
-    else 
-      nil
-    end 
-  
-  
+    self.find(name) or self.create_by_name(name)
   end 
   
   def self.find(name) 
