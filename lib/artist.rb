42adb8c093b49669aea53c246c349a63736bc7a2
Terry Threatt <terry.threatt@gmail.com>
2020-03-09 19:31:17 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require 'pry'
+class Artist 
+  attr_accessor :name, :songs 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs 
+    @songs
+  end 
+  
+  def add_song(song)
+    @songs << song 
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if @@all.find do |artist|
+      # binding.pry 
+      artist.name == name 
+      artist
+    end 
+    elsif  
+       artist = Artist.new(name)
+       @@all << artist
+       artist
+    else 
+      nil
+    end 
+  end 
+  
+  def print_songs
+    @songs.each do |song|
+      puts song.name  
+      end 
+  end 
+end 
